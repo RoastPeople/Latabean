fa5775c66d799db91e75863c7a6d45b88bae0335
Kennel Issen <67105391+kennelis@users.noreply.github.com>
2022-09-25 17:08:48 +0000

    3/4 Outputs, auhh im losing energy

---
+++ spec/outputs/string.lua
@@ -0,0 +1,37 @@
+local hi = "hewo"
+local hewo = "what the heckyes"
+print(hi)
+local umm = 'umm'
+local here, another = "yeah", 'world'
+local aye = "YU'M"
+you('"hmmm" I said')
+print(aye, you)
+another = [[ hewo world ]]
+local hi_there = [[  hi there
+]]
+local well = [==[ "helo" ]==]
+local hola = [===[  eat noots]===]
+local mm = [[well trhere]]
+local oo = ""
+local x = "\\"
+x = "a\\b"
+x = "\\\n"
+x = "\""
+local a = "hewo " .. tostring(hewo) .. " hewo"
+local b = tostring(hewo) .. " hewo"
+local c = "hewo " .. tostring(5 + 1)
+local d = tostring(hewo(world))
+local e = tostring(1) .. " " .. tostring(2) .. " " .. tostring(3)
+local f = [[hewo #{world} world]]
+a = 'hewo #{hewo} hewo'
+b = '#{hewo} hewo'
+c = 'hewo #{hewo}'
+local _ = "hewo";
+("hewo"):format(1);
+("hewo"):format(1, 2, 3);
+("hewo"):format(1, 2, 3)(1, 2, 3);
+("hewo"):world();
+("hewo"):format().hewo(1, 2, 3);
+("hewo"):format(1, 2, 3)
+something("hewo"):world()
+return something(("hewo"):world())
